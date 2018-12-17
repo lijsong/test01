ae68658c7a38b63000a015fe3f85fc6645126805
lijsong <li_jian_song@163.com>
2018-12-17 21:13:05 +0800

    第一次提交

---
+++ src/main/java/cn/itcast/demo/HelloWorld.java
@@ -0,0 +1,17 @@
+package cn.itcast.demo;
+
+/**
+ * @author: lijs
+ * @date: 2018/12/17
+ * @time: 21:06
+ * @Description:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello  world");
+
+        System.out.println("你好");
+    }
+}
