5a28af35ec82ebab47b9b239b90286a8aefbb2e7
lijsong <li_jian_song@163.com>
2018-12-17 21:24:34 +0800

    第一次提交

---
+++ src/main/java/cn/itcast/demo/demo01.java
@@ -0,0 +1,15 @@
+package cn.itcast.demo;
+
+/**
+ * @author: lijs
+ * @date: 2018/12/17
+ * @time: 21:23
+ * @Description:
+ */
+public class demo01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("111111111");
+    }
+}
